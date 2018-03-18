79c5219f404559687312c46871c556a9a4eb23cd
hpeddula <hpeddula@outlook.com>
2018-03-18 12:11:16 +0530

    Add employeeCount component and corresponding files

---
+++ src/app/employeeCount.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector :'emp-count',
+  templateUrl : './employeeCount.html',
+  // styleUrls:['./employeeCount.css']
+
+})
+
+export class EmployeeCountComponent{
+  All :number =5;
+  Male :number =6;
+  FeMale :number =7;
+}
